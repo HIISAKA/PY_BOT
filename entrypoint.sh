f71558c109f993042dea111f694b73948bba167c
Hisaka <67713406+HIISAKA@users.noreply.github.com>
2025-11-22 22:27:47 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Pull the latest changes from the cloned GitHub repository
-git -C /bot pull
+if git -C /bot pull; then
+  echo "Pulled latest changes from GitHub."
+else
+  echo "Failed to pull from GitHub repository." >&2
+  exit 1
+fi
 
 # Execute the Python bot
 exec python /bot/main.py
